c75dfaba6917d02f22cfec258a0a07293dd4be0f
alexander.fritz <alexander.fritz@renamesoftware.com>
2021-08-30 22:00:00 +0200

    mapus yeetus

--- map.py
+++ map.py
@@ -2,10 +2,12 @@ from PIL import Image
 from PyQt5 import QtGui
 import numpy as np
 import random
+from random import randrange
 import progressbar
 
 class map:
-    def generate():
+    def generate(self):
+        print('yeet')
         image = Image.open('map.png')
         image = image.convert('RGB')
 
@@ -27,6 +29,9 @@ class map:
                 max_value=image.width,
             ).start()
 
+        rany = self.numberlist(1, image.width - 2, 10, 30)
+        ranx = self.numberlist(1, image.height - 2, 10, 30)
+
         for x in range(image.width):
             for y in range(image.height):
                 r, g, b = image.getpixel((x,y))
@@ -34,13 +39,35 @@ class map:
                     pixels[x,y] = (255 + round(noise[random.randint(0, 99)]), 114 + round(noise[random.randint(0, 99)]), 00  + round(noise[random.randint(0, 99)]))
                 elif(r == 255 and g == 16 and b == 0): #MainStreet
                     pixels[x,y] = (255 + round(noise[random.randint(0, 99)]), 16 + round(noise[random.randint(0, 99)]), 0  + round(noise[random.randint(0, 99)]))
-                elif(r == 132 and g == 0 and b == 0): #water
-                    pixels[x,y] = (132 + round(noise[random.randint(0, 99)]), 0 + round(noise[random.randint(0, 99)]), 0  + round(noise[random.randint(0, 99)]))
                 elif(r == 0 and g == 0 and b == 0): #water
                     pixels[x,y] = (0 + round(noise[random.randint(0, 99)]), 0 + round(noise[random.randint(0, 99)]), 0  + round(noise[random.randint(0, 99)]))        
+                elif(r == 132 and g == 0 and b == 0): #CityStreet
+                    if(y in rany):
+                        pixels[x,y] = (255 ,255 ,255)
+                    elif(x in ranx):
+                        rany = self.numberlist(1, image.width - 2, 10, 30)
+                        pixels[x,y] = (255 ,255 ,255) 
+                    else:
+                        pixels[x,y] = (0 ,0 ,0) 
+
             bar.update(x + 1)
 
         bar.finish()
         data = processed.tobytes("raw","RGB")
         out = QtGui.QImage(data, processed.size[0], processed.size[1], QtGui.QImage.Format_RGB888)
+        processed.show()
         return out
+
+    def numberlist(min, max, mindistance, maxdistance):
+        ran = [min, max]
+        start = min
+        while(start + mindistance < max):
+            if(start + maxdistance > max):
+                ran.append((max - start) / 2 + start)
+                break
+            else:
+                start = randrange(start + mindistance, start + maxdistance)
+                ran.append(start)
+
+        ran.sort()
+        return ran
--- roads.py
+++ roads.py
@@ -1,15 +1,40 @@
 from PIL import Image
 from PyQt5 import QtGui
+from random import randrange
+import numpy as np
 
 class roads:
-    def generate():
-        road = Image.new('RGB', (1024, 1024), color=0)
-        pixels = road.load()
+    road = Image.new('RGB', (1024, 1024), color=0)
+    pixels = road.load()
 
-        for x in range(road.width):
-            for y in range(road.height):
-                pixels[x,y] = (x, 0, 0) 
+    def generate(self):
+        rany = self.numberlist(1, self.road.width - 2, 50, 100)
+        ranx = self.numberlist(1, self.road.height - 2, 50, 100)
+        for x in range(self.road.width):
+            for y in range(self.road.height):
+                if(y in rany):
+                    self.pixels[x,y] = (255 ,255 ,255)
+                elif(x in ranx):
+                    rany = self.numberlist(1, self.road.width - 2, 50, 100)
+                    self.pixels[x,y] = (255 ,255 ,255) 
+                else:
+                    self.pixels[x,y] = (0 ,0 ,0) 
+                
 
-        data = road.tobytes("raw","RGB")
-        road = QtGui.QImage(data, road.size[0], road.size[1], QtGui.QImage.Format_RGB888)
+        data = self.road.tobytes("raw","RGB")
+        road = QtGui.QImage(data, self.road.size[0], self.road.size[1], QtGui.QImage.Format_RGB888)
         return road
+
+    def numberlist(min, max, mindistance, maxdistance):
+        ran = [min, max]
+        start = min
+        while(start + mindistance < max):
+            if(start + maxdistance > max):
+                ran.append((max - start) / 2 + start)
+                break
+            else:
+                start = randrange(start + mindistance, start + maxdistance)
+                ran.append(start)
+
+        ran.sort()
+        return ran
