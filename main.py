acc77e3e3354522b0643a7a92d67a547b816704b
alexander.fritz <alexander.fritz@renamesoftware.com>
2021-08-31 23:18:18 +0200

    New Gui

--- main.py
+++ main.py
@@ -1,3 +1,72 @@
+from threading import Thread
 import map
+from PIL import Image
+from random import randrange
+import tkinter as tk
 
-map.generate()
+
+def number_list(minimum, maximum, min_distance, max_distance):
+    ran = [minimum, maximum]
+    start = minimum
+    while start + min_distance < maximum:
+        if start + max_distance > maximum:
+            ran.append((maximum - start) / 2 + start)
+            break
+        else:
+            start = randrange(start + min_distance, start + max_distance)
+            ran.append(start)
+
+    ran.sort()
+    return ran
+
+
+def map_gen():
+    image = Image.open('map.png')
+    image = image.convert('RGB')
+
+    images = []
+
+    thread_count = 100
+
+    threads = []
+
+    for i in range(thread_count):
+        thread = Thread(target=map.generate, args=(
+            thread_count, i, image, images))
+        threads.append(thread)
+        thread.start()
+
+    for t in threads:
+        t.join()
+
+    out = images[0]
+
+    for i in range(len(images)):
+        out.paste(images[i], (0, 0), images[i])
+
+    out.show()
+
+
+root = tk.Tk()
+canvas = tk.Canvas(root,
+                   width=500,
+                   height=500)
+canvas.pack()
+
+print(type(canvas))
+
+
+def add_pixel(x, y):
+    canvas.create_rectangle(x, y, x, y, outline="white")
+
+
+def test():
+    for i in range(500):
+        for j in range(500):
+            print(i, j)
+            add_pixel(i, j)
+
+
+button = tk.Button(root, text="yeet", command=test).pack()
+
+tk.mainloop()
--- map.py
+++ map.py
@@ -1,52 +1,16 @@
 from PIL import Image
 import numpy as np
 import random
-from random import randrange
-import progressbar
 
 
-def numberList(minimum, maximum, minDistance, maxDistance):
-    ran = [minimum, maximum]
-    start = minimum
-    while start + minDistance < maximum:
-        if start + maxDistance > maximum:
-            ran.append((maximum - start) / 2 + start)
-            break
-        else:
-            start = randrange(start + minDistance, start + maxDistance)
-            ran.append(start)
+def generate(threads, thread_number, image, images):
 
-    ran.sort()
-    return ran
-
-
-def generate():
-    print('yeet')
-    image = Image.open('map.png')
-    image = image.convert('RGB')
-
-    processed = Image.new('RGB', (image.width, image.height), color=0)
+    processed = Image.new('RGBA', (image.width, image.height), color=0)
     pixels = processed.load()
 
     noise = np.random.normal(0, 5, 100)
 
-    widgets = [
-        'Scanning Map',
-        progressbar.Bar(marker=progressbar.AnimatedMarker(
-            fill='█',
-        )),
-        progressbar.Percentage(),
-    ]
-
-    bar = progressbar.ProgressBar(
-        widgets=widgets,
-        max_value=image.width,
-    ).start()
-
-    randY = numberList(1, image.width - 2, 10, 30)
-    randX = numberList(1, image.height - 2, 10, 30)
-
-    for x in range(image.width):
+    for x in range(thread_number, image.width, threads):
         for y in range(image.height):
             r, g, b = image.getpixel((x, y))
             if r == 255 and g == 114 and b == 0:  # Land
@@ -59,17 +23,5 @@ def generate():
             elif r == 0 and g == 0 and b == 0:  # water
                 pixels[x, y] = (0 + round(noise[random.randint(0, 99)]), 0 + round(noise[random.randint(0, 99)]),
                                 0 + round(noise[random.randint(0, 99)]))
-            elif r == 132 and g == 0 and b == 0:  # CityStreet
-                if y in randY:
-                    pixels[x, y] = (255, 255, 255)
-                elif x in randX:
-                    randY = numberList(1, image.width - 2, 10, 30)
-                    pixels[x, y] = (255, 255, 255)
-                else:
-                    pixels[x, y] = (0, 0, 0)
 
-        bar.update(x + 1)
-
-    bar.finish()
-
-    processed.show()
+    images.append(processed)
